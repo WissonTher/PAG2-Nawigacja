c5bde160daf022a2cd7806942adf4665689f196e
Michał Młotkiewicz <167993494+R0118@users.noreply.github.com>
2025-10-22 14:16:40 +0200

    break when reached desired point

--- disktra.py
+++ disktra.py
@@ -13,31 +13,32 @@ class Graph:
         for w, wier in wierzcholki.items():
             self.wierzcholki[w] = w
 
-
-    def dijkstra(self, wyjsciowy):
-        wyjsciowy_id = self.wierzcholki.index(wyjsciowy)
-        Q = [float('inf')] * self.lenkr
-        Q[wyjsciowy_id] = 0
+    def dijkstra(self, poczatkowy, koncowy=None):
+        poczatkowy_id = self.wierzcholki.index(poczatkowy)
+        odl = [float('inf')] * self.lenkr
+        odl[poczatkowy_id] = 0
         S = [False] * self.lenkr
 
         for _ in range(self.lenkr):
             minodl = float('inf')
             u = None
             for i in range(self.lenkr):
-                if not S[i] and Q[i] < minodl:
-                    minodl = Q[i]
+                if not S[i] and odl[i] < minodl:
+                    minodl = odl[i]
                     u = i
 
             if u is None:
                 break
-
+                
             S[u] = True
-
             for v in range(self.lenkr):
                 if self.polaczenia[u][v] != 0 and not S[v]:
-                    alt = Q[u] + self.polaczenia[u][v]
-                    if alt < Q[v]:
-                        Q[v] = alt
-
-        return Q
+                    alt = odl[u] + self.polaczenia[u][v]
+                    if alt < odl[v]:
+                        odl[v] = alt
+             if koncowy is not None and u == self.wierzcholki.index(koncowy):
+                return odl[u]
+        if koncowy is not None:
+            return float('inf')
+        return odl
 
