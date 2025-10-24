ee5f135e49d6a106797b20783a35e6ebf1cf5939
BarneyGSN <marcinkacprzyk03@wp.pl>
2025-10-24 09:55:44 +0200

    Improved loading from file.

--- wierzcholki_drog.py
+++ wierzcholki_drog.py
@@ -2,40 +2,38 @@ import arcpy
 import numpy as np
 
 arcpy.env.workspace = r"./torun_cut"
-cursor = arcpy.SearchCursor('cut_torun.shp')
-wierzcholki = {}
-krawedzie = {}
-for row in cursor:
-    if len(wierzcholki) == 0:
-        wierzcholki[0] = (row.Shape.firstPoint.X, row.Shape.firstPoint.Y, row.x_kod)
-        wierzcholki[1] = (row.Shape.firstPoint.X, row.Shape.firstPoint.Y, row.x_kod)
 
-        krawedzie[row.FID] = [0, 1, 0]
-    else:
-        znaleziono = False
-        for w in list(wierzcholki):
-            nowe_id = len(wierzcholki)
-            if (row.Shape.firstPoint.X == wierzcholki[w][0]) and (row.Shape.firstPoint.Y == wierzcholki[w][1]):
-                wierzcholki[nowe_id] = (row.Shape.lastPoint.X, row.Shape.lastPoint.Y, row.x_kod)
-                krawedzie[row.FID] = [w, nowe_id, 0]
-                znaleziono = True
-                break
-            elif (row.Shape.lastPoint.X == wierzcholki[w][0]) and (row.Shape.lastPoint.Y == wierzcholki[w][1]):
-                wierzcholki[nowe_id] = (row.Shape.firstPoint.X, row.Shape.firstPoint.Y, row.x_kod)
-                krawedzie[row.FID] = [nowe_id, w, 0]
-                znaleziono = True
-                break
-        if not znaleziono:
-            nowe_id = len(wierzcholki)
-            wierzcholki[nowe_id] = (row.Shape.firstPoint.X, row.Shape.firstPoint.Y, row.x_kod)
-            wierzcholki[nowe_id + 1] = (row.Shape.lastPoint.X, row.Shape.lastPoint.Y, row.x_kod)
-            krawedzie[row.FID] = [nowe_id, nowe_id + 1, 0]
+def przetworz_plik(shapefile):
+    cursor = arcpy.SearchCursor('cut_torun.shp')
+    wierzcholki = {}
+    krawedzie = {}
 
-    for fid, (id_pocz, id_kon, _) in krawedzie.items():
-        p1 = wierzcholki[id_pocz]
-        p2 = wierzcholki[id_kon]
+    def znajdz_lub_dodaj(wierzcholki, cache, x, y, kod, prec = 4):
+        wspolrzedne = (round(x, prec), round(y, prec))
+        if wspolrzedne in cache:
+            return cache[wspolrzedne]
+        nowe_id = len(wierzcholki)
+        wierzcholki[nowe_id] = (x, y, kod)
+        cache[wspolrzedne] = nowe_id
+        return nowe_id
+
+    cache_xy = {}
+    for row in cursor:
+        p1 = [row.Shape.firstPoint.X, row.Shape.firstPoint.Y]
+        p2 = [row.Shape.lastPoint.X, row.Shape.lastPoint.Y]
+        kod = row.x_kod
+
+        id1 = znajdz_lub_dodaj(wierzcholki, cache_xy, p1[0], p1[1], kod)
+        id2 = znajdz_lub_dodaj(wierzcholki, cache_xy, p2[0], p2[1], kod)
+
         wektor = np.array([p2[0] - p1[0], p2[1] - p1[1]])
-        krawedzie[fid][2] = np.linalg.norm(wektor)
+        dlugosc = np.linalg.norm(wektor)
+        krawedzie[row.FID] = [id1, id2, dlugosc]
+
+    return wierzcholki, krawedzie
+
+wierzcholki, krawedzie = przetworz_plik('./torun_cut.shp')
+
 print(wierzcholki)
 print(krawedzie)
 
